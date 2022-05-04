e88d823729065dae0bb207f1a503411ad27d76d1
JurRodi <rodijurrien@gmail.com>
2022-05-04 22:32:10 +0200

    Added profile page

---
+++ profile.php
@@ -0,0 +1,21 @@
+<?php 
+
+    include_once(__DIR__."/bootstrap.php");
+
+    
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kwizin</title>
+    <link rel="stylesheet" href="styling/style.css">
+</head>
+<body>
+    
+
+    <?php include_once(__DIR__ . "/partials/nav.inc.php"); ?>
+</body>
+</html>
