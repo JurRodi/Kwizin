a60984af96a192e902955aa7f570a43a85a7ae13
JurRodi <rodijurrien@gmail.com>
2022-05-04 09:55:17 +0200

    Added nav and header

---
+++ partials/header.inc.php
@@ -0,0 +1,5 @@
+<header>
+    <div>
+        <a id="logo" href="feed.php">Home</a>
+    </div>
+</header>
---
+++ partials/nav.inc.php
@@ -0,0 +1,20 @@
+<nav>
+    <a href="connect.php">
+        <div class="nav-item">
+            <img id="connect" src="icons/reconnect.png" alt="reconnect-button">
+            <p class="nav-title">reconnect</p>
+        </div>
+    </a>
+    <a href="feed.php">
+        <div class="nav-item active">
+            <img id="home" src="icons/home.png" alt="reconnect-button">
+            <p class="nav-title">home</p>
+        </div>
+    </a>
+    <a href="profile.php">
+        <div class="nav-item">
+            <img id="profile" src="icons/profile.png" alt="reconnect-button">
+            <p class="nav-title">profile</p>
+        </div>
+    </a>
+</nav>
