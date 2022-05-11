ed7794c2320d96effd77aa9e125029980aca8265
JurRodi <rodijurrien@gmail.com>
2022-05-11 19:59:26 +0200

    Responsive nav

--- partials/nav.inc.php
+++ partials/nav.inc.php
@@ -1,20 +1,20 @@
 <nav>
     <a href="reconnect.php">
-        <div class="nav-item">
-            <img id="reconnect" src="icons/reconnect.png" alt="reconnect-button">
+        <div class="nav-item <?php if(basename($_SERVER['PHP_SELF']) === "reconnect.php"){ echo "active";} ?>">
+            <img id="reconnect" src="icons/reconnect.svg" alt="reconnect-button">
             <p class="nav-title">reconnect</p>
         </div>
     </a>
     <a href="feed.php">
-        <div class="nav-item active">
-            <img id="home" src="icons/home.png" alt="home-button">
+        <div class="nav-item <?php if(basename($_SERVER['PHP_SELF']) === "feed.php" || basename($_SERVER['PHP_SELF']) === "meal.php"){ echo "active";} ?>">
+            <img id="home" class="" src="icons/home.svg" alt="home-button">
             <p class="nav-title">home</p>
         </div>
     </a>
     <a href="profile.php">
-        <div class="nav-item">
-            <img id="profile" src="icons/profile.png" alt="profile-button">
-            <p class="nav-title">profile</p>
+        <div class="nav-item <?php if(basename($_SERVER['PHP_SELF']) === "profile.php"){echo "active";} ?>">
+            <img id="profile" class="<?php echo "active" ?>" src="icons/profile.svg" alt="profile-button">
+            <p class="nav-title">profiel</p>
         </div>
     </a>
 </nav>
