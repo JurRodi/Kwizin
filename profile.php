d8a95883b6c92e67614d1d44123a21fcd77afb6b
JurRodi <rodijurrien@gmail.com>
2022-05-17 11:08:35 +0200

    Added profile page

--- profile.php
+++ profile.php
@@ -2,7 +2,9 @@
 
     include_once(__DIR__."/bootstrap.php");
 
-    
+    $id = 1;
+    $user = User::getById($id);
+    $meals = Meal::getMealsByUser($id);
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -14,8 +16,51 @@
     <link rel="stylesheet" href="styling/style.css">
 </head>
 <body>
-    
-
     <?php include_once(__DIR__ . "/partials/nav.inc.php"); ?>
+
+    <div class="profile content-line ">
+        <div class="centered"><img class="avatar big" src="images/<?php echo $user['avatar'] ?>" alt="<?php echo $user['firstname'] ?>"></div>
+        <h2 class="centered"><?php echo $user['firstname'] ?></h2>
+        <div class="ratings centered">
+            <div><img src="icons" alt="chef-icon"></div>
+            <div><img src="icons" alt="ster-icon"></div>
+        </div>
+        <div class="centered"><a class="button" id="edit-profile" href="">Profiel bewerken</a></div>
+        <p><?php echo $user['bio'] ?></p>
+    </div>
+    <div class="content-line extraProfile">
+        <div class="favoriteMeals">
+            <h3 class="red">Favoriete gerechten</h3>
+            <ul>
+                <li><?php echo $user['favorites'] ?></li>
+            </ul>
+        </div>
+        <div class="cultures">
+            <h3 class="red">Cultuur</h3>
+            <ul>
+                <li><?php echo $user['culture'] ?></li>
+            </ul>
+        </div>
+    </div>
+    <div class="best-meals content-line">
+
+    </div>
+    <div class="own-meals content-line">
+        <div class="profile-heading">
+            <h3 class="red">Gerechten</h3>
+            <a href="uploadMeal.php"><img src="icons/Icon-add.svg" alt="add-icon"></a>
+        </div>
+        <div class="profile-meals">
+            <?php foreach($meals as $meal): ?>
+                <div class="profile-meal">
+                    <div class="meal-image"><img src="<?php echo $meal['image'] ?>" alt="<?php echo $meal['name'] ?>"></div>
+                    <div class="meal-content">
+                        <h4 class="red"><?php echo $meal['name'] ?></h4>
+                        <p><?php echo $meal['rating'] ?></p>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    </div>
 </body>
 </html>
