f12d3a3f11b5d574d4ebe0bd42b4eff635935ae1
JurRodi <rodijurrien@gmail.com>
2022-05-19 22:29:49 +0200

    Pop-up script

--- scripts/mealPopUp.js
+++ scripts/mealPopUp.js
@@ -1,11 +1,14 @@
 document.querySelector('#signUp-meal').addEventListener('click', function() {
     document.querySelector('.pop-up').style.display = 'block';
+    document.querySelector('.pop-up-bg').style.opacity = '0.5';
 });
 
 document.querySelector('#confirm-signUp-meal').addEventListener('click', function() {
     document.querySelector('.pop-up').style.display = 'none';
+    document.querySelector('.pop-up-bg').style.opacity = '1.0';
 });
 
 document.querySelector('#annulation').addEventListener('click', function() {
     document.querySelector('.pop-up').style.display = 'none';
+    document.querySelector('.pop-up-bg').style.opacity = '1.0';
 });
