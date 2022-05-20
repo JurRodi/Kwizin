2df02e3cd649f65f107fdc67468ce20e4ba76caf
JurRodi <rodijurrien@gmail.com>
2022-05-20 16:29:41 +0200

    Added welcome page

---
+++ welcome.php
@@ -0,0 +1,27 @@
+<?php 
+
+
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kwizin</title>
+    <link rel="stylesheet" href="styling/style.css">
+</head>
+<body id="welcome-body">
+    <div class="welcome">
+        <h2>Welkom bij</h2>
+        <img src="images/Kwizin_logo.png" alt="Kwizin-logo">
+        <p>Ontmoet nieuwe mensen bij een lekker etentje.</p>
+        <div class="centered"><a class="button" href="login.php">Inloggen</a></div>
+        <div class="centered"><a class="button" href="register.php">Registreren</a></div>
+        <img id="itsMe" src="images/itsMe.svg" alt="itsMe-logo">
+    </div>
+    <img id="fork" class="welcome-bg" src="images/fork.svg" alt="fork">
+    <img id="spoon" class="welcome-bg" src="images/spoon.svg" alt="spoon">
+    <img id="knife" class="welcome-bg" src="images/knife.svg" alt="knife">
+</body>
+</html>
