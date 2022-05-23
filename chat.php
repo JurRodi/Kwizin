57237cc3f04b9ac7daab7f80810e911c311d809a
JurRodi <rodijurrien@gmail.com>
2022-05-23 13:50:01 +0200

    Added styling for chat

--- chat.php
+++ chat.php
@@ -4,7 +4,7 @@
     Security::onlyLoggedInUsers();
 
     $user = User::getByEmail($_SESSION['email']);
-    //$connected = User::getChatUser($user['id'], $_GET['u']);
+    $connected = User::getChatUser($_SESSION['reconnect'][$_GET['u']], $user['id']);
 
 ?><!DOCTYPE html>
 <html lang="en">
@@ -16,22 +16,26 @@
     <link rel="stylesheet" href="styling/style.css">
 </head>
 <body>
-    <div class="floating-icons">
+    <div class="floating-icons floating-icons-meal">
         <a href="reconnect.php"><img class="icon" src="icons/Icon-arrow-black.svg" alt="arrow-button"></a>
+        <div>
+            <a href="#" class=""><img class="chat-reconnect" src="icons/Icon-euro.svg" alt="euro-icon"></a>
+            <a href="#" class=""><img class="chat-reconnect" src="icons/Icon-reconnect.png" alt="reconnect-icon"></a>
+        </div>
     </div>
-    <div>
-        <div class="meal-avatar"><img class="avatar big" src="images/<?php echo $reconnect['avatar'] ?>" alt="<?php echo $reconnect['firstname'] ?>"></div>
-        <p><?php echo $reconnect['firstname'] ?></p>
+    <div class="reconnect-details">
+        <div class="chat-avatar"><img class="avatar medium" src="images/<?php echo $connected['avatar'] ?>" alt="<?php echo $connected['firstname'] ?>"></div>
+        <h3 class="red chat-username"><?php echo $connected['firstname'] ?></h3>
         <p><?php //echo $online ?></p>
-        <a href="#" class="chat-icon"><img class="chat-reconnect" src="icons/Icon-euro.svg" alt="euro-icon"></a>
-        <a href="#" class="chat-icon"><img class="chat-reconnect" src="icons/Icon-reconnect.png" alt="reconnect-icon"></a>
     </div>
-    <div>
-        <div class=""><img class="" src="icons/Icon-camera.svg" alt="camera-icon"></div>
-        <div class=""><img class="" src="icons/Icon-photo-library.svg" alt="library-icon"></div>
-        <div class=""><img class="" src="icons/Icon-microphone.svg" alt="microphone-icon"></div>
-        <form action="" method="POST">
-            <input type="text" name="chat" class="chat" >
+    <div class="chat-footer">
+        <div class="chat-footer-icons">
+            <div class=""><img class="" src="icons/Icon-camera.svg" alt="camera-icon"></div>
+            <div class=""><img class="" src="icons/Icon-photo-library.svg" alt="library-icon"></div>
+            <div class=""><img class="" src="icons/Icon-microphone.svg" alt="microphone-icon"></div>
+        </div>
+        <form action="" method="POST" class="chat-form">
+            <input type="text" name="chat" placeholder="Aa" class="chat-input" >
         </form>
     </div>
 </body>
