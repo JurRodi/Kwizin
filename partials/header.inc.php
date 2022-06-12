d0ce05c174f97f731da84096c354e085c539539e
JurRodi <rodijurrien@gmail.com>
2022-06-12 13:10:45 +0200

    Minor img fix

--- partials/header.inc.php
+++ partials/header.inc.php
@@ -1,5 +1,5 @@
 <header>
     <div>
-        <a id="logo" href="feed.php"><img src="images/kwizin_logo.png" alt="logo"></a>
+        <a id="logo" href="feed.php"><img src="images/Kwizin_logo.png" alt="logo"></a>
     </div>
 </header>
